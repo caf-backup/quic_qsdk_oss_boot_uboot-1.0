996d048a463ddd0670799a7832c9dd776272f33d
Sanjeev N <nsanjeev@codeaurora.org>
2017-07-11 17:37:29 +0530

    ipq40xx: Adding support for DK07-C3
    This configuration is to support audio gateway
    in DK07.
    Features removed: eMMC and SPI NOR Flash
    Features added: WCD and Audio
    
    Change-Id: I1bdde7c07b7fe54712565a099cf77265a4f5c27e
    Signed-off-by: Sanjeev N <nsanjeev@codeaurora.org>

--- board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
+++ board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
@@ -428,6 +428,7 @@ void board_nand_init(void)
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK06_1_C1) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK07_1_C1) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK07_1_C2) ||
+		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK07_1_C3) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK04_1_C4) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK04_1_C5)) {
 
@@ -464,7 +465,8 @@ void board_nand_init(void)
 		spi_nand_init();
 	}
 
-	if (gboard_param->machid != MACH_TYPE_IPQ40XX_AP_DK07_1_C2) {
+	if ((gboard_param->machid != MACH_TYPE_IPQ40XX_AP_DK07_1_C2) &&
+	    (gboard_param->machid != MACH_TYPE_IPQ40XX_AP_DK07_1_C3)) {
 #ifdef CONFIG_IPQ40XX_SPI
 		ipq_spi_init(CONFIG_IPQ_SPI_NOR_INFO_IDX);
 #endif
@@ -621,6 +623,7 @@ int board_eth_init(bd_t *bis)
 		break;
 	case MACH_TYPE_IPQ40XX_AP_DK07_1_C1:
 	case MACH_TYPE_IPQ40XX_AP_DK07_1_C2:
+	case MACH_TYPE_IPQ40XX_AP_DK07_1_C3:
 		mdelay(1);
 		writel(GPIO_OUT, GPIO_IN_OUT_ADDR(41));
 		ipq40xx_register_switch(ipq40xx_qca8075_phy_init);
