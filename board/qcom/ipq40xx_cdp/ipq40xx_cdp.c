26a1c2355dcfa91d5cd0c1988c72f8d799328b67
Rajkumar Ayyasamy <arajkuma@codeaurora.org>
2018-04-11 12:15:00 +0530

    ipq40xx: Enabled spi-nor gpio init for DK04-C2 Board
    Change-Id: I5015e66e492f3fbc9e2ee7afc47b84fe01dd96c1
    Signed-off-by: Rajkumar Ayyasamy <arajkuma@codeaurora.org>

--- board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
+++ board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
@@ -430,6 +430,7 @@ void board_nand_init(void)
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK07_1_C1) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK07_1_C2) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK07_1_C3) ||
+		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK04_1_C2) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK04_1_C4) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK04_1_C5)) {
 
