fe40fcd8de4aa7f15e59a7f862ebe475e58189b8
Vinoth Gnanasekaran <vgnana@codeaurora.org>
2018-06-06 16:49:07 +0530

    ipq40xx: emmc: Fix a typo error
    Change-Id: I73f78b726b2ab51229f7eca30d45ae233d506bdf
    Signed-off-by: Vinoth Gnanasekaran <vgnana@codeaurora.org>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -1315,7 +1315,7 @@ int mmc_startup(struct mmc *mmc)
 	if (((mmc->cid[0] & MMC_MID_MASK) == MMC_MID_SANDISK) &&
 		(strncmp(mmc->block_dev.product, "SEM08", 5) == 0))
 		mmc->quirks |= MMC_QUIRK_SECURE_TRIM;
-	if (((mmc->cid[0] & MMC_MID_MASK) == MMC_MID_THOSIBA) &&
+	if (((mmc->cid[0] & MMC_MID_MASK) == MMC_MID_TOSHIBA) &&
 		(strncmp(mmc->block_dev.product, "004GE", 5) == 0))
 		mmc->quirks |= MMC_QUIRK_SECURE_TRIM;
 
